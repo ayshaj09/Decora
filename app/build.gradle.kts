a93b035c75ee26f9f8093b117a5bf2617ebf769d
aysha_j <ayshajamshaid060@gmail.com>
2025-10-29 22:29:40 +0500

    page 15 added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
