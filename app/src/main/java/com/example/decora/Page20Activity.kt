03ec5746be454ab3fb9c3f28ac30894e94e1633b
aysha_j <ayshajamshaid060@gmail.com>
2025-11-01 23:33:04 +0500

    fixes

--- app/src/main/java/com/example/decora/Page20Activity.kt
+++ app/src/main/java/com/example/decora/Page20Activity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Page20 : AppCompatActivity() {
+class Page20Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/example/decora/Page21Activity.kt
+++ app/src/main/java/com/example/decora/Page21Activity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Page19 : AppCompatActivity() {
+class Page21Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_page19)
+        setContentView(R.layout.activity_page21)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
